9cd2e3b4851f16063dd86b45fbfc4f0fc352de53
ritsyy <richarupela95@gmail.com>
2015-09-02 00:19:47 +0530

    Added Requirements

--- src/findAround/wsgi.py
+++ src/findAround/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "findAround.settings")
 
 application = get_wsgi_application()
+
+
+try:
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+except:
+	pass
